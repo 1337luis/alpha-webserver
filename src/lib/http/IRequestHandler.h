ab234a6cb816c7241bf8bf86ddd8122f4753091b
Luis Ángel Santos <luis1337@outlook.es>
2024-04-06 16:57:01 +0200

    Interface for request handlers

--- src/lib/http/IRequestHandler.h
+++ src/lib/http/IRequestHandler.h
@@ -4,18 +4,20 @@
  * @author Luis Santos <luis1337@outlook.es>
  * @date 2024-04-06
  */
+
 #ifndef H_HTTP_IREQUESTHANDLER
 #define H_HTTP_IREQUESTHANDLER
 
 #include "Request.h"
+#include "Response.h"
 
 namespace Http
 {
     class IRequestHandler
     {
     public:
-        virtual const char * getResponse();
-        virtual void setRequest( Request request );
+        virtual Response getResponse() = 0;
+        virtual void processRequest( Request request ) = 0;
     };
 }
 
