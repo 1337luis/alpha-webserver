214cb175f2e7e4105fd6ad4108d20cf3b9fcebaa
Luis Ángel Santos <luis1337@outlook.es>
2024-04-06 17:01:41 +0200

    Added handler to the server

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,24 @@
 /**
- * Main file
+ * Alpha WebServer - Custom HTTP Web Server implementation
  * 
  * @author Luis Santos <luis1337@outlook.es>
  * @date 2024-04-06
  */
 
 #include <iostream>
+
 #include "lib/tools/Logger.h"
 #include "lib/http/Server.h"
+#include "lib/http/DefaultRequestHandler.h"
+
+
+#include "lib/http/Request.h"
 
 int main( int argc, char* argv[] )
 {
-    Http::Server *server = new Http::Server( "0.0.0.0", 8080, nullptr );
+    Http::DefaultRequestHandler response_handler;
+    Http::Server *server = new Http::Server( "0.0.0.0", 8080, &response_handler );
+
     server->start();
 
     delete server;
