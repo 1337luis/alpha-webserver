aa48c0bf5ad11995a909c200307d44c90f43bcf2
Luis Ángel Santos <luis1337@outlook.es>
2024-04-10 22:09:16 +0200

    Add default response methods and clean code

--- src/lib/http/Response.cpp
+++ src/lib/http/Response.cpp
@@ -11,7 +11,7 @@ namespace Http
 {
     Response::Response()
     {
-
+        final_content = "HTTP/1.1 200 Ok\nServer:alphaWebServer\nContent-Type: text/html\nContent-Length: 14\n\n<h1>Hola!</h1>";
     }
 
     Response::~Response()
@@ -21,15 +21,34 @@ namespace Http
 
     int Response::length()
     {
-        return 97 * sizeof( char );
+        return final_content.length() * sizeof( char );
     }
 
     const char* Response::to_string()
     {
-        return "HTTP/1.1 200 Ok\nServer:alphaWebServer\nContent-Type: text/html\nContent-Length: 14\n\n<h1>Hola!</h1>";
+        return final_content.c_str();
     }
 
     Response Response::fromFile( std::string file_path )
+    {
+        Response r;
+        std::string base_path = Tools::FileSystem::getCurrentPath() + "/htdocs";
+
+        if( Tools::FileSystem::fileExists( base_path + file_path ) )
+        {
+            std::string file_content = Tools::FileSystem::fileGetContents( base_path + file_path );
+
+            // Build request content
+            r.final_content = "HTTP/1.1 200 Ok\nServer:alphaWebServer\nContent-Type: text/html\nContent-Length: " + std::to_string( file_content.length() );
+            r.final_content.append("\n\n" + file_content);
+        } else {
+            r.final_content = "HTTP/1.1 404 Not found\nServer:alphaWebServer\nContent-Type: text/html\nContent-Length: 31\n\n<h1>Error 404. Not found</h1>";
+        }
+        
+        return r;
+    }
+
+    Response Response::fromStatusCode( int status_code )
     {
 
     }
--- src/lib/http/Response.h
+++ src/lib/http/Response.h
@@ -11,6 +11,8 @@
 #include <string>
 #include <unordered_map>
 
+#include "../tools/FileSystem.h"
+
 namespace Http
 {
     class Response
@@ -22,55 +24,14 @@ namespace Http
         int length();
         const char* to_string();
 
-        Response fromFile( std::string file_path );
+        static Response fromFile( std::string file_path );
+        static Response fromStatusCode( int status_code );
+
     private:
         int status_code, content_length;
-        std::string content_type, content_type_charset;
+        bool isPlainContent = true;
 
-        const std::unordered_map<int, const char*> httpStatusMessages = {
-            { 100, "Continue" },
-            { 101, "Switching Protocols" },
-            { 200, "OK" },
-            { 201, "Created" },
-            { 202, "Accepted" },
-            { 203, "Non-Authoritative Information" },
-            { 204, "No Content" },
-            { 205, "Reset Content" },
-            { 206, "Partial Content" },
-            { 300, "Multiple Choices" },
-            { 301, "Moved Permanently" },
-            { 302, "Found" },
-            { 303, "See Other" },
-            { 304, "Not Modified" },
-            { 305, "Use Proxy" },
-            { 307, "Temporary Redirect" },
-            { 400, "Bad Request" },
-            { 401, "Unauthorized" },
-            { 402, "Payment Required" },
-            { 403, "Forbidden" },
-            { 404, "Not Found" },
-            { 405, "Method Not Allowed" },
-            { 406, "Not Acceptable" },
-            { 407, "Proxy Authentication Required" },
-            { 408, "Request Timeout" },
-            { 409, "Conflict" },
-            { 410, "Gone" },
-            { 411, "Length Required" },
-            { 412, "Precondition Failed" },
-            { 413, "Payload Too Large" },
-            { 414, "URI Too Long" },
-            { 415, "Unsupported Media Type" },
-            { 416, "Range Not Satisfiable" },
-            { 417, "Expectation Failed" },
-            { 418, "I'm a teapot" },
-            { 426, "Upgrade Required" },
-            { 500, "Internal Server Error" },
-            { 501, "Not Implemented" },
-            { 502, "Bad Gateway" },
-            { 503, "Service Unavailable" },
-            { 504, "Gateway Timeout" },
-            { 505, "HTTP Version Not Supported" }
-        };
+        std::string final_content, content_type, content_type_charset, plain_content;
 
         std::unordered_map<std::string, std::string> headers = {
             { "Server", "alpha WebServer" },
